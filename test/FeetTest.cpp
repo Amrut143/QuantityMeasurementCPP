0949f44f1a4e5fe8ea797457f98b8921f9c2e74a
Amrut <amrutpanda7789@gmail.com>
2020-09-23 19:21:52 +0530

    ADDED: Test to check the reference type when equal return true

--- test/FeetTest.cpp
+++ test/FeetTest.cpp
@@ -1,17 +1,22 @@
 #include "../main/model/Feet.h"
 #include <gtest/gtest.h>
 
-TEST(FeetTest, given0FeetAnd0FeetWhenCompareShouldReturnTrue) { 
+TEST(FeetTest, given_0Feet_And_0Feet_WhenCompare_ShouldReturnTrue) { 
     Feet first_zero_feet(0); 
     Feet second_zero_feet(0);
     ASSERT_EQ(first_zero_feet, second_zero_feet);
 }
 
-TEST(FeetTest, givenOneFeetAndNullFeetShouldReturnFalse) {
+TEST(FeetTest, givenOneFeetAndNullFeet_ShouldReturnFalse) {
     Feet first_one_feet(1);
     ASSERT_FALSE(first_one_feet == nullptr);
 }
 
+TEST(FeetTest, givenSameReference_WhenEqual_ShouldReturnTrue) {
+    Feet *first_ref = new Feet(1);
+    Feet *second_ref = first_ref;
+    ASSERT_EQ(first_ref, second_ref);
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
